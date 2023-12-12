8a57930922b0b38e73a4ea1a0178a2bce8e6b361
Tripti Garg <145544395+triptitsds@users.noreply.github.com>
2023-12-12 16:03:28 +0530

    Update my_dag.py

--- my_dag.py
+++ my_dag.py
@@ -1,13 +1,16 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from src.pipeline import run_pipeline, process_and_save_results
+from pipeline import run_pipeline, process_and_save_results
+import logging
+data_file_path = 'UIM_data.csv'
+dict_file_path = 'UIM_dictionary.xlsx'
 
 # Define DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2023, 12, 11),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -19,7 +22,8 @@ dag = DAG('my_dag', default_args=default_args, schedule_interval=timedelta(days=
 # Define tasks
 def run_and_save_results(**kwargs):
     results = run_pipeline(data_file_path, dict_file_path)
-    process_and_save_results(results)
+    output_path = process_and_save_results(results)
+    return output_path
 
 run_and_save_results_task = PythonOperator(
     task_id='run_and_save_results',
@@ -35,13 +39,16 @@ run_and_save_results_task  # No dependencies for this task as it's the starting 
 
 # Example: Data transformation process
 def data_transformation(**kwargs):
+    output_path = kwargs['output_path']
+    logging.info(f"CSV file saved at: {output_path}")
     # Your data transformation logic goes here
-    print("Data transformation step")
+    logging.info("Data transformation step")
 
 data_transformation_task = PythonOperator(
     task_id='data_transformation',
     python_callable=data_transformation,
     provide_context=True,
+    op_kwargs={'output_path': '{{ task_instance.xcom_pull(task_ids="run_and_save_results") }}'},
     dag=dag,
 )
 
